bcae302479308c8aa1ef98a659484a19c50d349d
Roboneet <neethumariyajoy@gmail.com>
2017-08-16 16:42:46 +0530

    zoom in easing

--- dist/js/scene1.js
+++ dist/js/scene1.js
@@ -44,7 +44,7 @@
        .to([skyEle,bushTree, carpet, lights], .3, fadeIn)
        .to(torch, .8, fadeIn)
        .to(fire, 0.7, fadeIn)
-       .to([stadium, carpet, grass, line_art, bushTree], 4, {scale: 60,yPercent:200, ease: Power4.easeOut})
+       .to([stadium, carpet, grass, line_art, bushTree], 4, {scale: 60,yPercent:200, ease: Expo.easeIn})
        .to(lights,.5, {autoAlpha:0}, '-=2')
        .to([tree],.1,{scale: 5, ease: Power4.easeOut}, '-=3');
 
