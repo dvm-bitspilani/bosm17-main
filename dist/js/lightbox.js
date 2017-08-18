9f6bf9203e6179f57301345cd148f4da5814c3c1
Sarthak Sehgal <teknosarthak@gmail.com>
2017-08-18 20:59:55 +0530

    Data entry

--- dist/js/lightbox.js
+++ dist/js/lightbox.js
@@ -2,60 +2,132 @@ var lightboxes = {
   "sports": {
     "basketball": {
       "image": "icons/basketball.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "bcapt": "Vishal Verma",
+      "bcno": "8146675797",
+      "gcapt": "Mitali Jain",
+      "gcno": "7877111996",
+      "mplayers": "12",
+      "pdf": ""
     },
     "chess": {
       "image": "icons/chess.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "mplayers": "5 (classical: 4 + 1 reserve), 5 (Blitz: 4 + 1 reserve), 4 (Problem Solving)",
+      "capt": "Simantini",
+      "cno": "9422729502",
+      "pdf": ""
     },
     "cricket": {
-      "image": "icons/basketball.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "image": "icons/cricket.png",
+      "bcapt": "Sri Harsha",
+      "bcno": "8058733360",
+      "mplayers": "15",
+      "pdf": ""
     },
     "powerlifting": {
       "image": "icons/power-lift.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "mplayers": " - ",
+      "capt": "Satrajit Nath",
+      "cno": "8860549691",
+      "pdf": ""
     },
     "athletics": {
       "image": "icons/athletics.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "bcapt": "Utkarsh Vashishtha",
+      "bcno": "9871483169",
+      "gcapt": "Harshita Verma",
+      "gcno": "7891055201",
+      "mplayers": " - ",
+      "pdf": ""
     },
     "badminton": {
       "image": "icons/badminton.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "bcapt": "Aman Agarwal",
+      "bcno": "9928066227",
+      "gcapt": "M Mahitha",
+      "gcno": "7073606977",
+      "mplayers": " 4 ",
+      "pdf": ""
     },
     "hockey": {
       "image": "icons/hockey.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "bcapt": "Prashant Rai",
+      "bcno": "8003359586",
+      "mplayers": " - ",
+      "pdf": ""
     },
     "squash": {
       "image": "icons/squash.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "bcapt": "Shreyas Verma",
+      "bcno": "9987295874",
+      "mplayers": "3+1",
+      "pdf": ""
     },
     "swimming": {
       "image": "icons/swimming.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "bcapt": "Prayag Katta",
+      "bcno": "8058967899",
+      "gcapt": "Mrudula",
+      "gcno": "8826034708",
+      "mplayers": " - ",
+      "pdf": ""
     },
     "tabletennis": {
       "image": "icons/table-tennis.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "bcapt": "Neel Joshi",
+      "bcno": "9561643458",
+      "gcapt": "Saima Rashid",
+      "gcno": "9983089614",
+      "mplayers": " 5",
+      "pdf": ""
     },
     "tennis": {
       "image": "icons/tennis.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "bcapt": "Surakshit Soni",
+      "bcno": "9001436050",
+      "gcapt": "Saniya Dasot",
+      "gcno": "9887770000",
+      "mplayers": " 4",
+      "pdf": ""
     },
     "taekwondo": {
       "image": "icons/tykondo.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "mplayers": " - ",
+      "capt": "Mitul",
+      "cno": "7838453778",
+      "pdf": ""
     },
     "football": {
       "image": "icons/football.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "bcapt": "Siddharth Kaushal",
+      "bcno": "8295572443",
+      "gcapt": "Prakriti Roy",
+      "gcno": "9773383506",
+      "mplayers": " 16",
+      "pdf": ""
     },
     "volleyball": {
       "image": "icons/volleyball.png",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    }
+      "bcapt": "Sahil",
+      "bcno": "9882641234",
+      "gcapt": "Shreya Pathak",
+      "gcno": "9983088851",
+      "mplayers": " 10",
+      "pdf": ""
+    },
+    "carrom": {
+      "image": "icons/carrom.png",
+      "capt": "Sai Sourya",
+      "cno": "9676196078",
+      "mplayers": " -",
+      "pdf": ""
+    },
+    "snooker": {
+      "image": "icons/snooker.png",
+      "capt": "Anirudh Tandon",
+      "cno": "9756800830",
+      "mplayers": " -",
+      "pdf": ""
+    },
   },
   "profshows": {
     "rahul": {
@@ -111,6 +183,7 @@ function launchEvent(name) {
   ev.style.background = "transparent";
   if(name=="rahul") name="Rahul Subramanian";
   if(name=="sumit") name="Sumit Anand";
+  if (type == "PROF SHOW")
   var html = '\
   <div id="sport-wrapper">\
     <div class="atheletics">\
@@ -120,6 +193,57 @@ function launchEvent(name) {
         <div class="col">\
           <div class="details">'
           +item.description+
+          '</div>\
+          <div class="sport">'+name.toUpperCase()+'</div>\
+        </div>\
+        <div class="heading">'+type+'</div>\
+        <img src="'+item.image+'" alt="'+name.toUpperCase()+'">\
+        <div class="border"></div>\
+      </div>\
+    </div>\
+  </div>\
+  ';
+  else
+  var html = '\
+  <div id="sport-wrapper">\
+    <div class="atheletics">\
+      <div class="main_div">\
+        <div class="col img" style="flex:0.3">\
+        </div>\
+        <div class="col">\
+          <div class="details">\
+            <strong><h2>Team captain details:</h2></strong>';
+  if(item.hasOwnProperty('bcapt') && item.hasOwnProperty('gcapt'))
+    html +=
+            '<ul>\
+              <li>Boys:<br>\
+              Name: '+item.bcapt+'<br>\
+              Contact No.: '+item.bcno+'\
+              </li><li>Girls:<br>\
+              Name: '+item.gcapt+'<br>\
+              Contact No.: '+item.gcno+'\
+              </li>\
+            </ul>\
+            <br><strong>Max. no. of players: </strong>'+item.mplayers;
+  else if(item.hasOwnProperty('bcapt') && !item.hasOwnProperty('gcapt'))            
+    html+= 
+          '<ul>\
+              <li>Boys:<br>\
+              Name: '+item.bcapt+'<br>\
+              Contact No.: '+item.bcno+'\
+              </li>\
+            </ul>\
+            <br><strong>Max. no. of players: </strong>'+item.mplayers;
+  else if(!item.hasOwnProperty('bcapt') && !item.hasOwnProperty('gcapt'))
+    html+=
+        '<ul>\
+              <li>\
+              Name: '+item.capt+'<br>\
+              Contact No.: '+item.cno+'\
+              </li>\
+            </ul>\
+            <br><strong>Max. no. of players: </strong>'+item.mplayers;
+    html+=
           '</div>\
           <div class="sport">'+name.toUpperCase()+'</div>\
         </div>\
