9f1a2e481357adde8734744331f380143b1e99cf
Chinmay Pandhare <pandharechinmay882@gmail.com>
2017-08-18 10:34:34 +0530

    Menu + Events

--- dist/js/lightbox.js
+++ dist/js/lightbox.js
@@ -1,66 +1,72 @@
 var lightboxes = {
   "sports": {
     "basketball": {
-      "image": "",
+      "image": "icons/basketball.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "chess": {
-      "image": "",
+      "image": "icons/chess.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "cricket": {
-      "image": "",
+      "image": "icons/basketball.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "powerlifting": {
-      "image": "",
+      "image": "icons/power-lift.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "athletics": {
-      "image": "",
+      "image": "icons/athletics.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "badminton": {
-      "image": "",
+      "image": "icons/badminton.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "hockey": {
-      "image": "",
+      "image": "icons/hockey.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "squash": {
-      "image": "",
+      "image": "icons/squash.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "swimming": {
-      "image": "",
+      "image": "icons/swimming.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "tabletennis": {
-      "image": "",
+      "image": "icons/table-tennis.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "tennis": {
-      "image": "",
+      "image": "icons/tennis.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "taekwondo": {
-      "image": "",
+      "image": "icons/tykondo.png",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    "football": {
+      "image": "icons/football.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     "volleyball": {
-      "image": "",
+      "image": "icons/volleyball.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
   },
   "profshows": {
     "rahul": {
-      "image": "",
-      "description": ""
+      "image": "icons/rahul.png",
+      "description": "Rahul Subramanian is a Tamilian engineer-turned-comedian who realized he was too good for the world of mechanics. The 29-year-old is a regular at all the leading comedy venues in the country, and recently came out with a fresh set of jokes, a solo set titled Kal Main Udega that makes fun of all his break-ups, and his time at B-school. The Mumbai-based comedian has been performing since 2014, as he broke into the scene by winning comedy competitions like Virgin Pants and Canvas Laugh Club - all India open-mics. Recently he played a role in AIB's web series Honest Engineering Campus Placements."
     },
     "sumit": {
-      "image": "",
-      "description": ""
+      "image": "icons/sumit.png",
+      "description": "Sumit Anand has been doing stand up for 4 years and has performed in London, Singapore, Malaysia and India, in order of preference to respective countries. Albeit he admits that he loves India the most (and money).<br><br>In other news, he has a Youtube channel.What people say about him:<br><br>\
+\"Extraordinaire potential in Judo\" - Class IV Teacher<br><br>\
+\"You were funnier last week\" - Repeat Audience Member<br><br>;)"
     }
   }
 }
@@ -98,10 +104,13 @@ function launchMenu() {
   initMenu();
 }
 
-function launchSport(name) {
-  var sport = launchSecondary();
-  var item = lightboxes.sports[name];
-  sport.style.background = "transparent";
+function launchEvent(name) {
+  var type = (lightboxes.sports.hasOwnProperty(name))?'SPORTS EVENT':'PROF SHOW';
+  var ev = launchSecondary();
+  var item = lightboxes.sports[name] || lightboxes.profshows[name];
+  ev.style.background = "transparent";
+  if(name=="rahul") name="Rahul Subramanian";
+  if(name=="sumit") name="Sumit Anand";
   var html = '\
   <div id="sport-wrapper">\
     <div class="atheletics">\
@@ -114,19 +123,15 @@ function launchSport(name) {
           '</div>\
           <div class="sport">'+name.toUpperCase()+'</div>\
         </div>\
-        <div class="heading">SPORT EVENT</div>\
-        <img src="../images/atheletics.png" alt="ATHELETICS">\
+        <div class="heading">'+type+'</div>\
+        <img src="'+item.image+'" alt="'+name.toUpperCase()+'">\
         <div class="border"></div>\
       </div>\
     </div>\
   </div>\
   ';
-  sport.innerHTML = html;
-  return sport;
-}
-
-function launchProfShow(artist) {
-
+  ev.innerHTML = html;
+  return ev;
 }
 
 function launchInfo() {
@@ -138,12 +143,22 @@ $(window).on("load",function(){
     launchMenu();
   });
   $(document).on("click", ".slider .item",function(e){
-    launchSport(e.target.getAttribute('name'));
+    launchEvent(e.target.getAttribute('name'));
+    $('.lightbox.primary .slider').addClass('fadeout');
+    // $('.lightbox.primary .slider').toggleClass('fadein');
   });
   $(document).on("click", ".lightbox #sport-wrapper",function(){
     $('.lightbox.secondary').remove();
+    $('.lightbox.primary .slider').removeClass('fadeout');
+    // $('.lightbox.primary .slider').toggleClass('fadein');
   });
   $(document).on("click", ".lightbox .close",function(){
-    $('.lightbox').remove();
+    if($('.lightbox.secondary').length == 0)
+      $('.lightbox').remove();
+    else $('.lightbox.secondary').remove();
+  });
+  $(document).on("mousewheel", ".lightbox.secondary", function(e,d){
+    e.target.scrollTop += ( d < 0 ? 1 : -1 ) * 10;
+    e.preventDefault();
   });
 });
